3f6067f73c8840fcef8e4389882b4e7cc43b24dc
Marcos Oliveira <marcos@terminalroot.com.br>
2019-01-22 18:59:32 -0200

    ok pronto

--- myfetch.sh
+++ myfetch.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 display_info(){
 
 	d_title=$USER@$HOSTNAME
@@ -17,13 +18,19 @@ display_info(){
 	d_memfree=$(echo "scale=2;$(cat /proc/meminfo | sed -n 2p | tr -d [A-Za-z:' '])" / 1000000 | bc)" MB"
 	d_arch=$(getconf LONG_BIT)"-bit"
 	d_browser=$(xdg-settings get default-web-browser | sed 's/userapp-//g;s/-.*//g;s/\..*//g')
+	d_char=$(expr length "$d_title"); qtd=
+	for i in $(seq 1 $d_char); do
+		qtd="$qtd─"
+	done
 }
 
 set_info(){
 display_info
 cat <<EOF
 
+
 $d_title
+$qtd
 Distro: ${d^}
 OS: $d_os
 Kernel Name: $d_kname
@@ -40,4 +47,84 @@ Architeture: $d_arch
 Browser default: ${d_browser^}
 EOF
 }
-set_info
+
+gentoo="
+           ':oxkxoc,.                 
+       .lOXWMMMMMWNK0xc'.             
+    .oKWMMMMMMMMMMWWNXX0Ox:.          
+  .dNMMMMMMMMMMMMMNXNNXKK00Okl.       
+ ,OMMMMMMMMMMMMMX0OkxxxKK00OOOOkc.    
+ x0MMMMMMMMMMMKOdllddc,xK00OOOkk0Kd.  
+ lxkKNWMMMMMMMMN0o;,ldOKK00OOOkkkkKWo 
+  'ldxO0XNWMMMMMMMWWNXXKK00OOOkkkxxNMo
+     .;ldkKWWMMMMWWNNXKK00OOOkkkxONMKd
+       .dWMMMMMMWWNNXXKK00OOOkk0NMXkd.
+     :0MMMMMMMMWWNNXXKK00OOO0XMWKko,  
+  .dWMMMMMMMMWWNNXXXKK00O0XWMXOdc.    
+ lWMMMMMMMWWWNNNXXKK00KNWWKkdl'       
+lMMMMMWWWWNNNXXXKKXNWMN0xlc'          
+oWMWWNNNNNXXXXNNWWXOxlc;.             
+.oKWMWWWWWWWNKOxocc;..                
+  ':lodoolc:::;'.                     
+    .';;;;'.                          
+                                      
+
+"
+
+ubuntu="
+                                    
+                          .;ccc;    
+                  ......  ;ccccc,   
+               ;ccccccccc. ,:c:,    
+          .,c;  ;cc:;;::cc;.....    
+         ,cccc:          .,:cccc,   
+        :ccc:'              ':ccc:  
+       ,ccc:.                .:ccc; 
+ .:cc:' ,cc'                  'cccc.
+ cccccc..cc.                        
+ .:cc:' ,cc'                  'cccc.
+       ,ccc:.                .:ccc; 
+        :cccc'              ':ccc:  
+         ,cccc:          .,:cccc,   
+          .,c;  ;cc:;;:ccc;.....    
+               ;ccccccccc. ,:c:,    
+                  ......  ;ccccc,   
+                          .;ccc;    
+                                    
+"
+
+outra="
+                                        
+              .;lxOO0Oxl,.              
+          'lkXN0kddddddKWNKkc.          
+       'xXKo;.         kWWWWNXXd'       
+     ;KNo.     dKX0o   NWWWW0 'xN0,     
+   .0Nl       :WWWWW' ,WWWWWl   .dNO.   
+  ;NK.        OWWWWN  dWWWWW.     'XN,  
+ ,N0    'odddxNWWWWNxxXWWWWNdddo;  .XN' 
+ XN.   'WWWWWWWWWWWWWWWWWWWWWWWWW:  'NK 
+cWd     ONWWWWWWWWWWWWWWWWWWWWWW0.   OW;
+dWc         .XWWWWX..0WWWWN.         dWo
+dWc    .,;::lWWWWW0;:NWWWWK::;,      dWo
+:Wd   :NWWWWWWWWWWWWWWWWWWWWWWWX.    OW;
+ XN.  ;NWWWWWWWWWWWWWWWWWWWWWWWK    ,WK 
+ 'NK.  .',;cWWWWWO;;NWWWWK;;;,.    .XN. 
+  ,NX'     :WWWWW: 'WWWWWo        ;NX'  
+   .kNd.   xWWWWN. cWWWWW'      .kWk.   
+     ,0Nx, NWWWW0  .x0Kk;     ;ONO'     
+       .oXNWWWWWl         'ckNKo.       
+          .:d0NWKkxxkkk0XN0d:.          
+               .:oxxxxo:.               
+                                        
+"
+
+display_info
+
+case "$d" in
+
+	"gentoo") distro="$gentoo" ;;
+	"ubuntu") distro="$ubuntu" ;;
+	*) distro="outra" ;;
+esac
+
+paste <(printf "%s" "$distro") <(set_info) | lolcat
